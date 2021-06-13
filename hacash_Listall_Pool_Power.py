6de70903a48fec75366b5c2a2fb48e80989e035d
Michael K. Dwyer <41527080+dwymi02@users.noreply.github.com>
2021-06-13 12:21:10 -0400

    Update hacash_Listall_Pool_Power.py
    Added code to handle exceptions
    Changed formatting

--- hacash_Listall_Pool_Power.py
+++ hacash_Listall_Pool_Power.py
@@ -9,6 +9,7 @@
 #|
 #| CHANGE LOG      : Version       Date                                              Description of code change
 #|                   -------    -----------    --------------------------------------------------------------------------------------------------
+#|                   0.1.1      Jun-12-2021    Added code to handle exceptions
 #|                   0.1.0      Jun-05-2021    Initial inception
 #+-----------------------------------------------------------------------------------------------------------------------------------------------+
 #*/
@@ -21,72 +22,75 @@ from html_table_parser.parser import HTMLTableParser
 
 
 def url_get_contents(url):
-    """ Opens a website and read its binary contents (HTTP Response Body) """
-    req = urllib.request.Request(url=url)
-    f = urllib.request.urlopen(req)
+	""" Opens a website and read its binary contents (HTTP Response Body) """
+	req = urllib.request.Request(url=url)
+	f = urllib.request.urlopen(req)
 
-    return f.read()
+	return f.read()
 
 
 def strip_out_header_data(html_table):
-    """ Handles embedded lists; strips out header data """
-    copylist = []
+	""" Handles embedded lists; strips out header data """
+	copylist = []
 
-    for inner_p in html_table:
-        for inner_p_ofp in inner_p:
-            for item in inner_p_ofp:
-                if item == '#' or item == 'Address' or item == 'Clients' or item == 'PeriodPowWorth' or item == 'FindBlocks/Coins' or item == 'CompleteRewards' or item == 'DeservedRewards' or item == 'UnconfirmedRewards':
-                    continue
-                else:
-                    copylist.append(item)
+	for inner_p in html_table:
+		for inner_p_ofp in inner_p:
+			for item in inner_p_ofp:
+				if item == '#' or item == 'Address' or item == 'Clients' or item == 'PeriodPowWorth' or item == 'FindBlocks/Coins' or item == 'CompleteRewards' or item == 'DeservedRewards' or item == 'UnconfirmedRewards':
+					continue
+				else:
+					copylist.append(item)
 
-    return copylist
+	return copylist
 
 
 def sum_period_pow_worth(copylist):
-    """ Parse list; Sum Period PoW Worth """
-    strslash      = "/"
-    j             = 0
-    totalpowworth = 0
-    #tmp           = ""
+	""" Parse list; Sum Period PoW Worth """
+	strslash      = "/"
+	j             = 0
+	totalpowworth = 0
 
-    for item in copylist:
+	for item in copylist:
 
-        if (item.__contains__(strslash)):
-            tmp = copylist[j-1]
-            totalpowworth += int(copylist[j-1])
-            j += 1
-        else:
-            j += 1
-            continue
+		if (item.__contains__(strslash)):
+			tmp = copylist[j-1]
+			totalpowworth += int(copylist[j-1])
+			j += 1
+		else:
+			j += 1
 
-    return totalpowworth
+			continue
+
+	return totalpowworth
 
 
 def main():
-    allhacpoolslist = ['http://95.165.168.191:3340', 'http://182.92.163.225:3340', 'http://3.101.62.234:3340', 'http://163.123.181.77:3340', 'http://104.217.254.247:3340', 'http://65.21.94.113:3340']
+	allhacpoolslist = ['http://95.165.168.191:3340', 'http://182.92.163.225:3340', 'http://3.101.62.234:3340', 'http://163.123.181.77:3340', 'http://104.217.254.247:3340', 'http://65.21.94.113:3340']
 
-    for urlitem in allhacpoolslist:
-        url = urlitem
-        xhtml = url_get_contents(url).decode('utf-8')
+	try:
+		for urlitem in allhacpoolslist:
+			url = urlitem
+			xhtml = url_get_contents(url).decode('utf-8')
 
-        p = HTMLTableParser()
-        p.feed(xhtml)
+			p = HTMLTableParser()
+			p.feed(xhtml)
 
-        copylist = strip_out_header_data(p.tables)
+			copylist = strip_out_header_data(p.tables)
 
-        totalpowworth = 0
+			totalpowworth = 0
 
-        totalpowworth = sum_period_pow_worth(copylist)
+			totalpowworth = sum_period_pow_worth(copylist)
 
-        print("Total PoW Power for Pool [" + urlitem + "]:")
+			print("Total PoW Power for Pool [" + urlitem + "]:")
 
-        """ Print totalpowworth without formatting """
-        #print(totalpowworth)
+			""" Print totalpowworth without formatting """
+			#print(totalpowworth)
 
-        print(str(("{:,}".format(totalpowworth))))
+			print(str(("{:,}".format(totalpowworth))))
+	except:
+		print("\nError occurred processing pool [" + urlitem + "]")
 
 if __name__ == '__main__':
-    main()
+	main()
 
-# End of code hacash_eu_Hacash_Pool_Power.py
+# End of code hacash_Listall_Pool_Power.py
